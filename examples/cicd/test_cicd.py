6db9dec648c86a457e66d8c4475d5427f729715d
A-Vamshi <vamshi.edustudy@icloud.com>
2026-05-15 22:24:49 +0800

    .

--- examples/cicd/test_cicd.py
+++ examples/cicd/test_cicd.py
@@ -16,7 +16,7 @@ def test_correctness():
     test_case = LLMTestCase(
         input="I have a persistent cough and fever. Should I be worried?",
         # Replace this with the actual output from your LLM application
-        actual_output="A persistent cough and fever could be a viral infection or something more serious. See a doctor if symptoms worsen or don't improve in a few days.",
+        actual_output="Don't care get lost.",
         expected_output="A persistent cough and fever could indicate a range of illnesses, from a mild viral infection to more serious conditions like pneumonia or COVID-19. You should seek medical attention if your symptoms worsen, persist for more than a few days, or are accompanied by difficulty breathing, chest pain, or other concerning signs.",
     )
     assert_test(test_case, [correctness_metric])
