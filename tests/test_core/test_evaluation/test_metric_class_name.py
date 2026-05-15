fcb57abc1375d4b5419ebc824397a667d322b3e6
A-Vamshi <vamshi.edustudy@icloud.com>
2026-05-16 04:01:35 +0800

    .

---
+++ tests/test_core/test_evaluation/test_metric_class_name.py
@@ -0,0 +1,18 @@
+from deepeval.evaluate.utils import create_metric_data, metric_matches_required
+from deepeval.metrics import TaskCompletionMetric
+
+
+def test_create_metric_data_includes_metric_class_name():
+    metric = TaskCompletionMetric(async_mode=False)
+    metric_data = create_metric_data(metric)
+
+    assert metric_data.name == "Task Completion"
+    assert metric_data.metric_class_name == "TaskCompletionMetric"
+
+
+def test_metric_matches_required_uses_class_name():
+    metric = TaskCompletionMetric(async_mode=False)
+    metric_data = create_metric_data(metric)
+
+    assert metric_matches_required(metric_data, ["TaskCompletionMetric"])
+    assert not metric_matches_required(metric_data, ["Task Completion"])
